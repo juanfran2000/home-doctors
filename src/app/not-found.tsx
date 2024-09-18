2fe81335e16f839a410480d43eb2d3f5b225ffbc
jortegaruiz <jortegaruiz@gmail.com>
2024-09-17 22:57:54 -0300

    notFound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,37 +1,7 @@
-import BoxMail from "./components/BoxMail";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import ImportantData from "./components/ImportantData";
-import International from "./components/International";
-import Nav from "./components/Nav";
-import Out from "./components/Out";
-import Services from "./components/Services";
-
 export default function NotFound() {
   return (
-    <div>
-      <header>
-        <BoxMail />
-        <Nav />
-      </header>
-      <main>
-        <Hero />
-        <div id="home">
-          <ImportantData />
-        </div>
-        <div className="hidden lg:block bg-blue-100" id="our">
-          <Out />
-        </div>
-        <div id="services">
-          <Services />
-        </div>
-        <div id="countries" className="bg-blue-100">
-          <International />
-        </div>
-      </main>
-      <footer id="contact" className="bg-sky-950 py-20">
-        <Footer />
-      </footer>
+    <div className="flex justify-center mt-10 text-black">
+      <h4>Lo sentimos no pudimos cargar el lenguaje</h4>
     </div>
   );
 }
--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,7 +2,6 @@ import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
 import {routing} from './routing';
 
-const locales = ['en','es'];
  
 export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
